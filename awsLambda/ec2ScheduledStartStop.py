179c1bd83c447277447f430078d4ae9ccca6a28f
enes-13 <enesunal.6522@gmail.com>
2021-02-22 18:13:46 -0500

    lambda and cdn additions

---
+++ awsLambda/ec2ScheduledStartStop.py
@@ -0,0 +1,12 @@
+import boto3, json
+
+session = boto3.Session(profile_name='development')
+east = 'us-east-1'
+west = 'us-west-2'
+
+ec2Client = session.client('ec2', region_name = east)
+ec2Resource = session.resource('ec2', region_name=east)
+
+#regions = [region['RegionName']
+response=ec2Client.describe_regions()
+print(response['Regions'])
